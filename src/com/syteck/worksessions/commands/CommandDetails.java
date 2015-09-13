c1218a16dd05b20c92924b9dd10c7527a93baed1
SyTeck <syteckhd@gmail.com>
2015-09-13 15:01:12 +0200

    Finished commands

--- src/com/syteck/worksessions/commands/CommandDetails.java
+++ src/com/syteck/worksessions/commands/CommandDetails.java
@@ -1,19 +1,52 @@
 package com.syteck.worksessions.commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.syteck.worksessions.Session;
+import com.syteck.worksessions.StorageManager;
+import com.syteck.worksessions.User;
+import com.syteck.worksessions.utils.Util;
 
 public class CommandDetails implements Command {
 
 	@Override
 	public void execute(CommandSender sender, String[] args) {
-		// TODO Auto-generated method stub
-		
+
+		if(verify(sender, args)) {
+			
+			Player player = (Player) sender;
+			User user = StorageManager.getUser(player.getUniqueId());
+			Session session = user.getSession();
+			
+			player.sendMessage(ChatColor.GOLD+"Name: "+ChatColor.YELLOW+session.getName() + " ("+session.getId()+")");
+			player.sendMessage(ChatColor.GOLD+"Trusted: "+ChatColor.YELLOW+""+session.isTrusted());
+			player.sendMessage(ChatColor.GOLD+"Closed: "+ChatColor.YELLOW+""+session.isClosed());
+			player.sendMessage(ChatColor.GOLD+"Manager: "+ChatColor.YELLOW+""+Bukkit.getOfflinePlayer(session.getManager().getUUID()).getName());
+			player.sendMessage("       ");
+			player.sendMessage(ChatColor.GOLD+"There are currently "+ChatColor.YELLOW+session.getPlayerList().size()+ChatColor.GOLD+" players in the session.");
+			
+			if(Util.hasPermission(sender, "ws.assist")) {
+				
+				
+				
+			}
+		}
 	}
 
 	@Override
 	public boolean verify(CommandSender sender, String[] args) {
-		// TODO Auto-generated method stub
-		return false;
+
+		if(!StorageManager.getUser(((Player)sender).getUniqueId()).hasSession()) {
+
+			sender.sendMessage(ChatColor.RED+"You are not currently in a session.");
+
+			return false;
+		}
+
+		return true;
 	}
 
 }
--- src/com/syteck/worksessions/commands/CommandTpall.java
+++ src/com/syteck/worksessions/commands/CommandTpall.java
@@ -1,19 +1,56 @@
 package com.syteck.worksessions.commands;
 
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.syteck.worksessions.Session;
+import com.syteck.worksessions.StorageManager;
+import com.syteck.worksessions.utils.Util;
 
 public class CommandTpall implements Command {
 
 	@Override
 	public void execute(CommandSender sender, String[] args) {
-		// TODO Auto-generated method stub
 		
+		if(verify(sender, args)) {
+			
+			Player player = (Player) sender;
+			Session session = StorageManager.getUser(player.getUniqueId()).getSession();
+			
+			for(UUID uuid: session.getPlayerList()) {
+				
+				Player target = Bukkit.getPlayer(uuid);
+				
+				target.teleport(player.getLocation());
+				target.sendMessage(ChatColor.GREEN+"You have been teleported to your manager.");
+			}
+			
+			player.sendMessage(ChatColor.GREEN+"Teleported all session members to you.");
+		}
 	}
 
 	@Override
 	public boolean verify(CommandSender sender, String[] args) {
-		// TODO Auto-generated method stub
-		return false;
+
+		if(!Util.hasPermission(sender, "ws.assist")) {
+
+			sender.sendMessage(ChatColor.RED+"You do not have permission to do this.");
+
+			return false;
+		}
+		
+		if(!StorageManager.getUser(((Player)sender).getUniqueId()).hasSession()) {
+
+			sender.sendMessage(ChatColor.RED+"You are not currently in a session.");
+
+			return false;
+		}
+		
+		return true;
 	}
 
 }
